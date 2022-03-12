5d7d9186a558696d5d47c7c22c2e8c6172710ebf
bonkbanan <59296163+bonkbanan@users.noreply.github.com>
2022-03-12 20:31:26 +0200

    xex2

---
+++ src/main/java/banan/edu/model/Record.java
@@ -0,0 +1,70 @@
+package banan.edu.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Record {
+    private String id;
+    private String message;
+    private LocalDateTime dateTime;
+
+    public Record(String id, String message, LocalDateTime dateTime) {
+        this.id = id;
+        this.message = message;
+        this.dateTime = dateTime;
+    }
+
+    public Record(String message, LocalDateTime dateTime) {
+        this.message = message;
+        this.dateTime = dateTime;
+    }
+
+    public Record() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Record record = (Record) o;
+        return getId().equals(record.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Record{" +
+                "id='" + id + '\'' +
+                ", message='" + message + '\'' +
+                ", dateTime=" + dateTime +
+                '}';
+    }
+}
